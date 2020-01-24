6c29f9c6473afe46b8b3a8966a2d86f08a006a52
konparu-shinya <komikadoken@gmail.com>
2020-01-24 12:01:17 +0900

    パルスモーターコントローラーL6470に対応

--- gtnaction.c
+++ gtnaction.c
@@ -1,5 +1,7 @@
 /********************************************************************************
  * gtn for ラズパイ のAction実行部
+ *  パルスモーターコントローラー:L6470
+ *  gcc -o test test.c -lwiringPi
  ********************************************************************************/
 #include <stdio.h>
 #include <stdint.h>
@@ -12,9 +14,17 @@
 #include <time.h>
 #include <net/if.h>
 #include <sys/wait.h>
+#include <wiringPi.h>
+#include <wiringPiSPI.h>
+
+#define L6470_SPI_CHANNEL 0
+#define	GPIO17	17
+#define	GPIO18	18
+const unsigned char L6470_CH[] = {GPIO17, GPIO18, 0};
 
 #define QUEUELIMIT 5
 #define	CONSOLE_MAX	20
+#define	PPM_MAX	10
 
 #define ACK		0x06
 #define NAK		0x15
@@ -46,7 +56,6 @@ struct _seq_tbl {
 		{0,0,0,0,0,0,0},{0,0,0,0,0,0,0},{0,0,0,0,0,0,0},{0,0,0,0,0,0,0},{0,0,0,0,0,0,0}
 	};
 
-
 // 動作シーケンス格納テーブル
 struct _action_tbl {
 	int line;
@@ -63,6 +72,36 @@ struct _action_tbl {
 	int dummy2;
 	int dummy3;
 } static action_tbl[CONSOLE_MAX][1000];
+
+// パルスモーター管理テーブル
+struct _ppm_ctrl {
+	int driving;			// 0:not use 1:init home out 2:init home in 3:init home add 4:home/step 5:free
+	int	ratio;
+	struct _init_pulse {
+		int	init;
+		int	home_out;
+		int	home_in;
+		int	home_add;
+	} init_pulse;
+	struct _init_dir {
+		int	home_out;
+		int	home_in;
+	} init_dir;
+	struct _dir {
+		int home;
+		int step;
+	} dir;
+	struct _speed {
+		int	start;
+		int	max;
+		int	acc;
+		int	dec;
+	} speed;
+	int r9;
+	int r10;
+	int r11;
+} static ppm_ctrl[PPM_MAX]={0,0,0,0,0,0,0,0,0,0};
+
 
 // イベント
 static int event[20]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
@@ -145,6 +184,149 @@ static int message(int sock, int my_thread_no, int disp_no, int line_no, char *s
 	return 0;
 }
 
+// L6470に1byte送信
+static unsigned char L6470_write(unsigned char ch, unsigned char data)
+{
+	unsigned char org=data;
+	digitalWrite(L6470_CH[ch], 0);
+	wiringPiSPIDataRW(L6470_SPI_CHANNEL, &data, 1);
+	digitalWrite(L6470_CH[ch], 1);
+	delay(1);
+	return data;
+}
+
+// L6470にコマンド送信送信
+static long L6470_cmd_write(unsigned char ch, unsigned char no, char bytes, long data)
+{
+	unsigned char c;
+	int i;
+	long ret=0L;
+	L6470_write(ch, no);
+	for (i=bytes-1; i>=0; i--) {
+		c = L6470_write(ch, (unsigned char)(data>>(8*i)));
+		ret += ((long)c << (8*i));
+	}
+	return ret;
+}
+
+// L6470 パラメータ書き込み
+static long L6470_param_write(unsigned char ch, unsigned char no, long data)
+{
+/* パラメータ毎のbit数定義 */
+const int bit_tbl[] = {22, 9, 22, 20, 12, 12, 10, 13, 8, 8, 8, 8, 14, 8, 8, 8, 4, 5, 4, 7, 10, 8, 8, 16, 16};
+	int bytes = (bit_tbl[(no&0x1f)-1]+7)/8;
+	return L6470_cmd_write(ch, no, bytes, data);
+}
+
+// L6470 パラメータ読み出し
+static long L6470_param_read(unsigned char ch, unsigned char no)
+{
+	return L6470_param_write(ch, 0x20|no, 0L);
+}
+
+static void L6470_softstop(unsigned char ch)
+{
+	L6470_write(ch, 0xB0);
+}
+
+static void L6470_softhiz(unsigned char ch)
+{
+	L6470_write(ch, 0xA8);
+}
+
+// L6470 駆動確認
+static int L6470_busy(unsigned char ch)
+{
+	return ((L6470_param_read(ch, 0x19)&0x02) == 0x00)?1:0;
+}
+
+// L6470 初期設定
+static void L6470_init(unsigned char ch)
+{
+	// NOP x4 -> Reset
+	L6470_write(ch, 0);
+	L6470_write(ch, 0);
+	L6470_write(ch, 0);
+	L6470_write(ch, 0);
+	L6470_write(ch, 0xc0);
+	L6470_softstop(ch);
+	L6470_softhiz(ch);
+
+	// MIN_SPEED設定。
+	L6470_param_write(ch, 0x08, 0x15);
+	// MAX_SPEED設定。
+	L6470_param_write(ch, 0x07, 2000);
+	// KVAL_HOLD設定。
+	L6470_param_write(ch, 0x09, 0x0f);
+	// KVAL_RUN設定。
+	L6470_param_write(ch, 0x0A, 0xff);
+	// KVAL_ACC設定。
+	L6470_param_write(ch, 0x0B, 0xff);
+	// KVAL_DEC設定。
+	L6470_param_write(ch, 0x0C, 0xff);
+	// OCD_TH設定。
+	L6470_param_write(ch, 0x13, 0x0f);
+	// STALL_TH設定。
+	L6470_param_write(ch, 0x14, 0x7f);
+	// STEP MODE
+	L6470_param_write(ch, 0x16, ppm_ctrl[ch].r9);
+}
+
+// L6470 スピード変更
+static void L6470_change_spd(unsigned char ch, int start_pulse, int max_pulse, int st_slope, int ed_slope)
+{
+	struct _ppm_ctrl *pctrl = &ppm_ctrl[ch];
+	if (pctrl->speed.start!=start_pulse) {
+		pctrl->speed.start=start_pulse;
+		// MIN_SPEED設定。
+		L6470_param_write(ch, 0x08, start_pulse);
+	}
+	if (pctrl->speed.max!=max_pulse) {
+		pctrl->speed.max=max_pulse;
+		// MAX_SPEED設定。
+		L6470_param_write(ch, 0x07, max_pulse);
+	}
+	if (pctrl->speed.acc!=st_slope) {
+		pctrl->speed.acc=st_slope;
+		// ACC設定。
+		L6470_param_write(ch, 0x05, st_slope);
+	}
+	if (pctrl->speed.dec!=ed_slope) {
+		pctrl->speed.dec=ed_slope;
+		// DEC設定。
+		L6470_param_write(ch, 0x06, ed_slope);
+	}
+}
+
+// パルスモーターの初期化動作
+// 戻値: 0:初期化完了 1:初期化中
+static int ppm_init(int no)
+{
+	if (L6470_busy(no)) return 1;
+
+	switch (ppm_ctrl[no].driving) {
+	case 1:		// 1:init home out
+		// home out完了?
+		ppm_ctrl[no].driving=2;
+		break;
+	case 2:		// 2:init home in
+		// home in完了?
+		ppm_ctrl[no].driving=3;
+		break;
+	case 3:		// 3:init home add
+		// home add完了?
+		ppm_ctrl[no].driving=4;
+		break;
+	default:
+		// 初期化開始
+		// センサーIN?
+		ppm_ctrl[no].driving=1;
+		ppm_ctrl[no].driving=2;
+		break;
+	}
+	return (ppm_ctrl[no].driving==4) ? 0:1;
+}
+
 // シーケンス
 static int sequence(int sock, int no)
 {
@@ -176,25 +358,71 @@ static int sequence(int sock, int no)
 		}
 		break;
 	case 0x11:		// パルスモーター初期化
-	case 0x12:		// パルスモーターCW(相対パルス指定)
-	case 0x13:		// パルスモーターCCW(相対パルス指定)
+		if (ppm_init(pact->mno-1)==0) {
+			pseq->current++;
+		}
+		break;
+	case 0x12:		// パルスモーターSTEP(相対パルス指定)
+		if (!L6470_busy(pact->mno-1)) {
+			unsigned char cmd = (ppm_ctrl[pact->mno-1].dir.step)?0x40:0x41;
+			L6470_change_spd(pact->mno-1, pact->start_pulse, pact->max_pulse, pact->st_slope, pact->ed_slope);
+			L6470_cmd_write(pact->mno-1, cmd, 3, pact->move_pulse);//Move
+//			delay(10);
+			pseq->current++;
+		}
+		break;
+	case 0x13:		// パルスモーターHOME(相対パルス指定)
+		if (!L6470_busy(pact->mno-1)) {
+			unsigned char cmd = (ppm_ctrl[pact->mno-1].dir.home)?0x40:0x41;
+			L6470_change_spd(pact->mno-1, pact->start_pulse, pact->max_pulse, pact->st_slope, pact->ed_slope);
+			L6470_cmd_write(pact->mno-1, cmd, 3, pact->move_pulse);//Move
+//			delay(10);
+			pseq->current++;
+		}
+		break;
 	case 0x14:		// パルスモーターHOME(現在位置パルス分をCCW回転する)
+		if (!L6470_busy(pact->mno-1)) {
+			L6470_change_spd(pact->mno-1, pact->start_pulse, pact->max_pulse, pact->st_slope, pact->ed_slope);
+			L6470_cmd_write(pact->mno-1, 0x70, 1, 0);//GoHome
+//			delay(10);
+			pseq->current++;
+		}
+		break;
 	case 0x15:		// パルスモーター絶対パルス移動
+		if (!L6470_busy(pact->mno-1)) {
+			L6470_change_spd(pact->mno-1, pact->start_pulse, pact->max_pulse, pact->st_slope, pact->ed_slope);
+			L6470_cmd_write(pact->mno-1, 0x60, 3, pact->move_pulse);//GoTo
+//			delay(10);
+			pseq->current++;
+		}
+		break;
 	case 0x16:		// パルスモーター停止までまつ
+		if (!L6470_busy(pact->mno-1)) {
+			pseq->current++;
+		}
+		break;
 	case 0x17:		// パルスモーター励磁解除
+		// KVAL_HOLD設定。
+		L6470_param_write(pact->mno-1, 0x09, 0x0);
+		pseq->current++;
+		break;
 	case 0x18:		// パルスモーター励磁ON
+		// KVAL_HOLD設定。
+		L6470_param_write(pact->mno-1, 0x09, 0x0f);
+		pseq->current++;
+		break;
 //	case 0x31:		// パルスモーターI/O指定ビットON
 //	case 0x32:		// パルスモーターI/O指定ビットOFF
 //	case 0x33:		// パルスモーターI/O指定ビットをONまでまつ
 //	case 0x34:		// パルスモーターI/O指定ビットをOFFまでまつ
 //	case 0x35:		// パルスモーターI/O 16bitデータ書き込み
 //	case 0x36:		// パルスモーターI/O 16bitデータ読み込んで表示
+#if 0
 		{
 			/* パルスモーター停止までまつ */
 			if (pact->act==0x16) {
 				// 何もしない
 			}
-#if 0
 			/* Action */
 			else if (can_action_send(can, pseq->current, mno, pact)) {
 				sprintf(str, "ERR 行番号 = %d CAN Error", pact->line);
@@ -204,10 +432,10 @@ static int sequence(int sock, int no)
 			else{
 				pseq->slv_busy[pact->slvno]=1;
 			}
-#endif
 			pseq->current++;
 		}
 		break;
+#endif
 	case 0x41:		// DIO指定ビットON
 	case 0x42:		// DIO指定ビットOFF
 	case 0x43:		// DIO指定ビットをONまでまつ
@@ -343,6 +571,7 @@ static int sequence(int sock, int no)
         }
 		break;
 	case 0x71:		// A/D取り込み
+		pseq->current++;
 		break;
 	default:
 		pseq->current++;
@@ -404,25 +633,28 @@ static int local_reg_flag[CONSOLE_MAX]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
 
 	// パルスモーター設定
 	if (id==0xC101 && run_flag==0) {
-#if 0
-		if (can_ppm_conf_send(buf)) {
-			local_param_err = -1;
-		}
-		else{
-			local_param_err = 0;
-		}
-#endif
+		unsigned char no = (unsigned char)buf[7];
+		struct _ppm_ctrl *pctrl = &ppm_ctrl[no-1];
+		pctrl->ratio               = ((int)((unsigned char)buf[10])<<8) + (int)((unsigned char)buf[11]);
+		pctrl->init_pulse.init     = ((int)((unsigned char)buf[ 8])<<8) + (int)((unsigned char)buf[ 9]);
+		pctrl->init_pulse.home_out = ((int)((unsigned char)buf[12])<<8) + (int)((unsigned char)buf[13]);
+		pctrl->init_pulse.home_in  = 0;
+		pctrl->init_pulse.home_add = ((int)((unsigned char)buf[14])<<8) + (int)((unsigned char)buf[15]);
+		pctrl->init_dir.home_out   = (int)((unsigned char)buf[17]);
+		pctrl->init_dir.home_in    = (int)((unsigned char)buf[18]);
+		pctrl->dir.home            = (int)((unsigned char)buf[20]);
+		pctrl->dir.step            = (int)((unsigned char)buf[19]);
+		pctrl->r9   = ((int)((unsigned char)buf[21])<<24) + ((int)((unsigned char)buf[22])<<16) + ((int)((unsigned char)buf[23])<<8) + (int)((unsigned char)buf[24]);
+		pctrl->r10  = ((int)((unsigned char)buf[25])<<24) + ((int)((unsigned char)buf[26])<<16) + ((int)((unsigned char)buf[27])<<8) + (int)((unsigned char)buf[28]);
+		pctrl->r11  = ((int)((unsigned char)buf[29])<<24) + ((int)((unsigned char)buf[30])<<16) + ((int)((unsigned char)buf[31])<<8) + (int)((unsigned char)buf[32]);
+		pctrl->speed.start =0;
+		pctrl->speed.max   =0;
+		pctrl->speed.acc   =0;
+		pctrl->speed.dec   =0;
+		L6470_init(no-1);
 	}
 	// DCモーター設定
 	else if (id==0xC103 && run_flag==0) {
-#if 0
-		if (can_dc_conf_send(buf)) {
-			local_param_err = -1;
-		}
-		else{
-			local_param_err = 0;
-		}
-#endif
 	}
 	// 動作シーケンス
 	else if (id==0xC102 && run_flag==0) {
@@ -447,8 +679,11 @@ static int local_reg_flag[CONSOLE_MAX]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
 		struct _seq_tbl *pseq = &seq_tbl[no-1];
 		if (pseq->run==0) {
 			local_reg_flag[no-1]=pseq->reg_flag;			// レジスタ値を保存
-			memset(pseq, 0, sizeof(struct _seq_tbl));
+			memset(pseq, 0, sizeof(seq_tbl));
 			memset(event, 0, sizeof(event));
+			for (i=0; i<PPM_MAX; i++) {
+				ppm_ctrl[i].driving=0;
+			}
 			message(sock, no, 1, 1, "START");
 			message(sock, no, 1, 2, "");
 			message(sock, no, 1, 3, "");
@@ -500,6 +735,16 @@ static int local_reg_flag[CONSOLE_MAX]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
 			sprintf(str, "STOP 行番号 = %d", pact->line);
 			message(sock, no, 1, 1, str);
 		}
+		// パルスモーター強制停止
+		for (i=0; ;i++) {
+			if (L6470_CH[i]>0) {
+				L6470_softstop(i);
+				L6470_softhiz(i);
+			}
+			else{
+				break;
+			}
+		}
 	}
 	// スレッド生成
 	else if (id==0xC012) {
@@ -514,35 +759,27 @@ static int local_reg_flag[CONSOLE_MAX]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
 static int execute(int sock)
 {
 	char buf[512], str[32];
-	int i;
+	int i, len=1;
 
-	while (1) {
+	while (len!=0) {
 
 		// Local Sokcet
-		{
+		while (1) {
 			// STX受信 0:Close -1:nothing
 			char c;
-			int len = recv(sock, &c, 1, 0);
+			len = recv(sock, &c, 1, 0);
+
+			if (len<1) {
+				break;
+			}
 
 			// Recv
-			if (len>0) {
-				memset(buf, 0, sizeof(buf));
-				len = nt_recv(sock, c, buf);
-		printf("%s %d %d %02X %02X\n", __FILE__,__LINE__,len,(unsigned char)buf[4],(unsigned char)buf[5]);
-			}
-
+			memset(buf, 0, sizeof(buf));
+			len = nt_recv(sock, c, buf);
 			// ACK返信
-			if (len>0) {
-				nt_send(sock, ack_msg_data, sizeof(ack_msg_data));
-			}
-
+			nt_send(sock, ack_msg_data, sizeof(ack_msg_data));
 			// コマンド割り振り
-			if (len>0) {
-				dispatch(sock, buf);
-			}
-
-			// Close
-			if (len==0) break;
+			dispatch(sock, buf);
 		}
 
 		// シーケンス
@@ -563,7 +800,22 @@ int main(void)
 	struct sockaddr_in clitSockAddr;		//client internet socket address
 	unsigned int clitLen;					//client internet socket address length
 	unsigned short servPort = 9001;			//server port number
-	int on = 1, ret;
+	int on = 1, ret, i;
+
+	/* SPI channel 0 を 1MHz で開始 */
+	if (wiringPiSPISetupMode(L6470_SPI_CHANNEL, 1000000, 3) < 0) {
+		perror("SPI Setup failed:\n");
+		exit(EXIT_FAILURE);
+	}
+
+	if (wiringPiSetupGpio()==-1) {
+		perror("GPIO Setup failed:\n");
+		exit(EXIT_FAILURE);
+	}
+	for (i=0; L6470_CH[i]; i++) {
+		pinMode(L6470_CH[i], OUTPUT);
+		digitalWrite(L6470_CH[i], 1);
+	}
 
 	if ((servSock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0 ){
 		perror("socket() failed.");
