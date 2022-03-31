720de034a47a38e3ae8a3077642c9e4b7f7e455a
Konparu-Shinya <komikadoken@gmail.com>
2022-03-31 18:31:54 +0900

    シャッター駆動追加

--- LED電流調整.rb
+++ LED電流調整.rb
@@ -19,6 +19,10 @@ Led_conf = "#{ENV['HOME']}/Desktop/LED設定.txt"
 
 $led_conf = []
 
+$shutter_run = nil
+$shutter_value = [0, 0]
+
+
 $fact_a = 0.0256
 $fact_b = -28.084
 
@@ -40,7 +44,7 @@ def temp2hex(a)
 end
 
 class Window
-  attr_accessor :spi, :entAna, :lblLVal, :lblLA, :lblTVal, :lblTVal2, :lblCount, :lblTime, :lblTm2, :lblErr, :time_st, :time_st2, :entLED, :cbOnOff, :cbOverLed, :lblPumpVal, :lblMeasDt
+  attr_accessor :spi, :entAna, :lblLVal, :lblLA, :lblTVal, :lblTVal2, :lblCount, :lblTime, :lblTm2, :lblErr, :time_st, :time_st2, :entLED, :cbOnOff, :cbOverLed, :lblPumpVal, :lblMeasDt, :lblStSts, :entDelay, :entCntTime, :entShtPos, :btnStop
 
   def initialize
     @spi = WiringPiSpi.new
@@ -99,19 +103,42 @@ class Window
     @entCntTime.set_xalign(1)
     lblUnit3   = Gtk::Label.new("x0.1msec(0-99)");
     @lblLedErr = Gtk::Label.new("");
-    lblMeasSt1  = Gtk::Label.new("測光開始1");
+
+    lblShut   = Gtk::Label.new("シャッタ：");
+    @lblStSts = Gtk::Label.new("停止中");
+    btnInit   = Gtk::Button.new('イニシャル')
+    @rbtn1     = Gtk::RadioButton.new('全開')
+    @rbtn2     = Gtk::RadioButton.new(@rbtn1, '全閉')
+    @rbtn3     = Gtk::RadioButton.new(@rbtn1, '原点')
+    hbox      = Gtk::HBox.new()
+    hbox.pack_start(@rbtn1)
+    hbox.pack_start(@rbtn2)
+    hbox.pack_start(@rbtn3)
+    @rbtn3.active = true
+    @btnStop  = Gtk::Button.new('シャッタ動作')
+    lblInit   = Gtk::Label.new("開閾値");
+    @entInit  = Gtk::Entry.new
+    @entInit.set_text("5")
+    @entInit.set_xalign(1)
+    lblShtPos = Gtk::Label.new("シャッタ");
+    lblUnit5   = Gtk::Label.new("x0.1msec(0-99)");
+    @entShtPos  = Gtk::Entry.new
+    @entShtPos.set_text("5")
+    @entShtPos.set_xalign(1)
+
+    lblMeasSt1  = Gtk::Label.new("測光 1開始");
     @entMeasSt1 = Gtk::Entry.new
     @entMeasSt1.set_text("10")
     @entMeasSt1.set_xalign(1)
-    lblMeasEd1  = Gtk::Label.new("測光終了1");
+    lblMeasEd1  = Gtk::Label.new("~終了");
     @entMeasEd1 = Gtk::Entry.new
     @entMeasEd1.set_text("10")
     @entMeasEd1.set_xalign(1)
-    lblMeasSt2  = Gtk::Label.new("測光開始2");
+    lblMeasSt2  = Gtk::Label.new("測光 2開始");
     @entMeasSt2 = Gtk::Entry.new
     @entMeasSt2.set_text("10")
     @entMeasSt2.set_xalign(1)
-    lblMeasEd2  = Gtk::Label.new("測光終了2");
+    lblMeasEd2  = Gtk::Label.new("~終了");
     @entMeasEd2 = Gtk::Entry.new
     @entMeasEd2.set_text("10")
     @entMeasEd2.set_xalign(1)
@@ -126,7 +153,7 @@ class Window
     @lblTVal  = Gtk::Label.new("-");
     lblTMPCfg1a  = Gtk::Label.new("温度係数1a");
     @entTMPCfg1a = Gtk::Entry.new
-    lblTMPCfg1b  = Gtk::Label.new("温度係数1b");
+    lblTMPCfg1b  = Gtk::Label.new("1b");
     @entTMPCfg1b = Gtk::Entry.new
     @entTMPCfg1a.set_text("#{$fact_a}")
     @entTMPCfg1a.set_xalign(1)
@@ -135,7 +162,7 @@ class Window
     lblTMPCfg2a  = Gtk::Label.new("温度係数2a");
     @entTMPCfg2a = Gtk::Entry.new
     @lblTVal2 = Gtk::Label.new("-");
-    lblTMPCfg2b  = Gtk::Label.new("温度係数2b");
+    lblTMPCfg2b  = Gtk::Label.new("2b");
     @entTMPCfg2b = Gtk::Entry.new
     @entTMPCfg2a.set_text("#{$fact2_a}")
     @entTMPCfg2a.set_xalign(1)
@@ -193,6 +220,10 @@ class Window
             @entMeasSt2.set_text("#{ary[1]}") if ary[1]
           when 'meas_ed2'
             @entMeasEd2.set_text("#{ary[1]}") if ary[1]
+          when 'shutter_init'
+            @entInit.set_text("#{ary[1]}") if ary[1]
+          when 'shutter_pos'
+            @entShtPos.set_text("#{ary[1]}") if ary[1]
           end
         end
       end
@@ -208,7 +239,7 @@ class Window
 
     # pump on/off
     value = @entPumpCfg.text.to_i
-	value = 15 if value > 15
+    value = 15 if value > 15
    #@spi.dataRW([0x2f,0x40,0x80|((value>>6)&0x7), 0xc0|(value&0x3f)])
     @spi.dataRW([0x2f,0x40,0x80|(value&0xf), 0xc0])
 
@@ -301,22 +332,97 @@ class Window
       @spi.meas_points(@entMeasSt1.text.to_i, @entMeasEd1.text.to_i, @entMeasSt2.text.to_i, @entMeasEd2.text.to_i)
     end
 
+    # シャッタイニシャル
+    btnInit.signal_connect('clicked') do 
+      shutter_init
+      ary = @spi.dataRW([0x16,0x40,0x80,0xc0])
+      @btnStop.set_label('停止') if ary[0]==1 && (ary[1]&0x08)!=0x00
+    end
+
+    @entShtPos.signal_connect('changed') do 
+      ary = @spi.dataRW([0x16,0x40,0x80,0xc0])
+      if ary[0]==1 && (ary[1]&0x09)==0x01 && ($shutter_value[1]-$shutter_value[0])==25
+        # シャッタ動作開始
+        value = @entShtPos.text.to_i
+        #value = @entDelay.text.to_i + @entCntTime.text.to_i
+        @spi.dataRW([0x36,0x41,0x80|((value>>6)&0x3f),0xc0|(value&0x3f)])
+        @lblStSts.set_text("Run 開2:#{$shutter_value[1]} 開1:#{$shutter_value[0]}")
+        style = Gtk::Style.new
+        style.set_fg(Gtk::STATE_NORMAL, 0, 0, 65535)
+        @lblStSts.style = style
+
+        @btnStop.set_label('停止')
+      end
+    end
+
+    # シャッタ停止
+    @btnStop.signal_connect('clicked') do 
+      ary = @spi.dataRW([0x16,0x40,0x80,0xc0])
+      if ary[0]==1 && (ary[1]&0x09)==0x00 && ($shutter_value[1]-$shutter_value[0])!=25
+        # シャッタイニシャル
+        shutter_init
+        ary = @spi.dataRW([0x16,0x40,0x80,0xc0])
+        @btnStop.set_label('停止') if ary[0]==1 && (ary[1]&0x08)!=0x00
+
+      elsif ary[0]==1 && (ary[1]&0x09)==0x00 && ($shutter_value[1]-$shutter_value[0])==25
+        # シャッタ動作開始
+        value = @entShtPos.text.to_i
+        #value = @entDelay.text.to_i + @entCntTime.text.to_i
+        @spi.dataRW([0x36,0x41,0x80|((value>>6)&0x3f),0xc0|(value&0x3f)])
+        @lblStSts.set_text("Run 開2:#{$shutter_value[1]} 開1:#{$shutter_value[0]}")
+        style = Gtk::Style.new
+        style.set_fg(Gtk::STATE_NORMAL, 0, 0, 65535)
+        @lblStSts.style = style
+
+        @btnStop.set_label('停止')
+
+      elsif ary[0]==1 && (ary[1]&0x08)==0x00
+        # 原点停止
+        if @rbtn3.active?
+          @spi.dataRW([0x36,0x42,0x80,0xc0])
+          @lblStSts.set_text('停止中')
+          style = Gtk::Style.new
+          style.set_fg(Gtk::STATE_NORMAL, 0, 0, 0)
+          @lblStSts.style = style
+          @btnStop.set_label('シャッタ動作')
+        # 全開停止
+        elsif @rbtn1.active? && (($shutter_value[1]-$shutter_value[0])==25)
+          value = ($shutter_value[0] + 6) * 4 / 5
+          @spi.dataRW([0x36,0x42,0x80|((value>>6)&0x3f),0xc0|(value&0x3f)])
+          @lblStSts.set_text('停止中')
+          style = Gtk::Style.new
+          style.set_fg(Gtk::STATE_NORMAL, 0, 0, 0)
+          @lblStSts.style = style
+          @btnStop.set_label('シャッタ動作')
+        # 全閉停止
+        elsif (($shutter_value[1]-$shutter_value[0])==25)
+          value = ($shutter_value[1] > 1) ? ($shutter_value[1] - 2) * 4 / 5 : 0
+          @spi.dataRW([0x36,0x42,0x80|((value>>6)&0x3f),0xc0|(value&0x3f)])
+          @lblStSts.set_text('停止中')
+          style = Gtk::Style.new
+          style.set_fg(Gtk::STATE_NORMAL, 0, 0, 0)
+          @lblStSts.style = style
+          @btnStop.set_label('シャッタ動作')
+        end
+      end
+    end
+
     @entPumpCfg.signal_connect('changed') do 
       # pump on/off
       value = @entPumpCfg.text.to_i
      #@spi.dataRW([0x2f,0x40,0x80|((value>>6)&0x7), 0xc0|(value&0x3f)])
-	  value = 15 if value > 15
+      value = 15 if value > 15
       @spi.dataRW([0x2f,0x40,0x80|(value&0xf), 0xc0])
     end
 
     tbl = Gtk::Table.new(2,3,true)
     tbl.set_column_spacings(3) 
     tbl.attach_defaults(@entAna,    0, 5, 0, 1)
-    tbl.attach_defaults(lblAna,     5, 7, 0, 1)
+    tbl.attach_defaults(lblAna,     5, 6, 0, 1)
     tbl.attach_defaults(lblLED,     0, 2, 1, 2)
     tbl.attach_defaults(@entLED,    2, 5, 1, 2)
-    tbl.attach_defaults(@lblLVal,   6, 7, 1, 2)
-    tbl.attach_defaults(@lblLA,     7, 8, 1, 2)
+    tbl.attach_defaults(@lblLVal,   5, 6, 1, 2)
+    tbl.attach_defaults(@lblLA,     6, 7, 1, 2)
     tbl.attach_defaults(lbtn,       0, 1, 2, 3)
     tbl.attach_defaults(@scrl,      1, 7, 2, 3)
     tbl.attach_defaults(rbtn,       7, 8, 2, 3)
@@ -329,51 +435,73 @@ class Window
     tbl.attach_defaults(@lblTm2,    6, 8, 5, 6)
     tbl.attach_defaults(@lblErr,    6, 8, 6, 7)
 
-    tbl.attach_defaults(lblGT,      0, 2, 8, 9)
-    tbl.attach_defaults(@entGT,     2, 5, 8, 9)
-    tbl.attach_defaults(lblGTUnit,  5, 6, 8, 9)
-    tbl.attach_defaults(lblLED_ON,  0, 2, 9,10)
-    tbl.attach_defaults(@entLED_ON, 2, 5, 9,10)
-    tbl.attach_defaults(lblUnit1,   5, 7, 9,10)
-    tbl.attach_defaults(lblDelay,   0, 2,10,11)
-    tbl.attach_defaults(@entDelay,  2, 5,10,11)
-    tbl.attach_defaults(lblUnit2,   5, 7,10,11)
-    tbl.attach_defaults(lblCntTime, 0, 2,11,12)
-    tbl.attach_defaults(@entCntTime,2, 5,11,12)
-    tbl.attach_defaults(lblUnit3,   5, 7,11,12)
-    tbl.attach_defaults(lblMeasSt1, 0, 2,12,13)
-    tbl.attach_defaults(@entMeasSt1,2, 5,12,13)
-    tbl.attach_defaults(lblMeasEd1, 0, 2,13,14)
-    tbl.attach_defaults(@entMeasEd1,2, 5,13,14)
-    tbl.attach_defaults(lblMeasSt2, 0, 2,14,15)
-    tbl.attach_defaults(@entMeasSt2,2, 5,14,15)
-    tbl.attach_defaults(lblMeasEd2, 0, 2,15,16)
-    tbl.attach_defaults(@entMeasEd2,2, 5,15,16)
-    tbl.attach_defaults(@lblMeasDt, 2, 4,16,17)
-    tbl.attach_defaults(@lblLedErr, 2, 7,17,18)
+    tbl.attach_defaults(lblGT,      0, 2, 7, 8)
+    tbl.attach_defaults(@entGT,     2, 5, 7, 8)
+    tbl.attach_defaults(lblGTUnit,  5, 6, 7, 8)
+    tbl.attach_defaults(lblLED_ON,  0, 2, 8, 9)
+    tbl.attach_defaults(@entLED_ON, 2, 5, 8, 9)
+    tbl.attach_defaults(lblUnit1,   5, 7, 8, 9)
+    tbl.attach_defaults(lblDelay,   0, 2, 9,10)
+    tbl.attach_defaults(@entDelay,  2, 5, 9,10)
+    tbl.attach_defaults(lblUnit2,   5, 7, 9,10)
+    tbl.attach_defaults(lblCntTime, 0, 2,10,11)
+    tbl.attach_defaults(@entCntTime,2, 5,10,11)
+    tbl.attach_defaults(lblUnit3,   5, 7,10,11)
 
-    tbl.attach_defaults(lblTMP,      0, 2,18,19)
-    tbl.attach_defaults(@entTMP,     2, 5,18,19)
-    tbl.attach_defaults(lblDo,       5, 6,18,19)
-    tbl.attach_defaults(@lblTVal,    6, 7,18,19)
-    tbl.attach_defaults(lblTMPCfg1a, 0, 2,19,20)
-    tbl.attach_defaults(@entTMPCfg1a,2, 5,19,20)
-    tbl.attach_defaults(lblTMPCfg1b, 0, 2,20,21)
-    tbl.attach_defaults(@entTMPCfg1b,2, 5,20,21)
-    tbl.attach_defaults(lblTMPCfg2a, 0, 2,21,22)
-    tbl.attach_defaults(@entTMPCfg2a,2, 5,21,22)
-    tbl.attach_defaults(@lblTVal2,   6, 7,21,22)
-    tbl.attach_defaults(lblTMPCfg2b, 0, 2,22,23)
-    tbl.attach_defaults(@entTMPCfg2b,2, 5,22,23)
+    tbl.attach_defaults(lblShut,     0, 2,12,13)
+    tbl.attach_defaults(@lblStSts,   2, 5,12,13)
+    tbl.attach_defaults(btnInit,     0, 2,13,14)
+    tbl.attach_defaults(hbox,        2, 6,13,14)
+    tbl.attach_defaults(@btnStop,    6, 8,13,14)
+    tbl.attach_defaults(lblInit,     0, 2,14,15)
+    tbl.attach_defaults(@entInit,    2, 5,14,15)
+    tbl.attach_defaults(lblShtPos,   0, 2,15,16)
+    tbl.attach_defaults(@entShtPos,  2, 5,15,16)
+    tbl.attach_defaults(lblUnit5,    5, 7,15,16)
 
-    tbl.attach_defaults(lblPumpCfg,  0, 2,24,25)
-    tbl.attach_defaults(@entPumpCfg, 2, 5,24,25)
-    tbl.attach_defaults(lblUnit4,    5, 7,24,25)
-    tbl.attach_defaults(@lblPumpVal, 6, 7,25,26)
+    tbl.attach_defaults(lblMeasSt1,  0, 2,17,18)
+    tbl.attach_defaults(@entMeasSt1, 2, 5,17,18)
+    tbl.attach_defaults(lblMeasEd1,  5, 6,17,18)
+    tbl.attach_defaults(@entMeasEd1, 6, 9,17,18)
+    tbl.attach_defaults(lblMeasSt2,  0, 2,18,19)
+    tbl.attach_defaults(@entMeasSt2, 2, 5,18,19)
+    tbl.attach_defaults(lblMeasEd2,  5, 6,18,19)
+    tbl.attach_defaults(@entMeasEd2, 6, 9,18,19)
+    tbl.attach_defaults(@lblMeasDt,  2, 4,19,20)
+    tbl.attach_defaults(@lblLedErr,  2, 7,20,21)
+
+    tbl.attach_defaults(lblTMP,      0, 2,21,22)
+    tbl.attach_defaults(@entTMP,     2, 5,21,22)
+    tbl.attach_defaults(lblDo,       5, 6,21,22)
+    tbl.attach_defaults(@lblTVal,    6, 7,21,22)
+    tbl.attach_defaults(lblTMPCfg1a, 0, 2,22,23)
+    tbl.attach_defaults(@entTMPCfg1a,2, 5,22,23)
+    tbl.attach_defaults(lblTMPCfg1b, 5, 6,22,23)
+    tbl.attach_defaults(@entTMPCfg1b,6, 9,22,23)
+    tbl.attach_defaults(@lblTVal2,   9,10,22,23)
+    tbl.attach_defaults(lblTMPCfg2a, 0, 2,23,24)
+    tbl.attach_defaults(@entTMPCfg2a,2, 5,23,24)
+    tbl.attach_defaults(lblTMPCfg2b, 5, 6,23,24)
+    tbl.attach_defaults(@entTMPCfg2b,6, 9,23,24)
+
+    tbl.attach_defaults(lblPumpCfg,  0, 2,25,26)
+    tbl.attach_defaults(@entPumpCfg, 2, 5,25,26)
+    tbl.attach_defaults(lblUnit4,    5, 7,25,26)
+    tbl.attach_defaults(@lblPumpVal, 8, 9,25,26)
 
     win.add(tbl)
     win.show_all()
 
+  end
+
+  def shutter_init
+    ary = @spi.dataRW([0x16,0x40,0x80,0xc0])
+    if ary[0]==1 && (ary[1]&0x08)==0x00
+      set_led_value
+      value = @entInit.text.to_i
+      @spi.dataRW([0x36,0x48,0x80|((value>>6)&0x3f),0xc0|(value&0x3f)])
+      $shutter_run = 0
+    end
   end
 
   def set_led_value
@@ -436,7 +564,7 @@ p [__LINE__, @spi.dataRW([0x29,0x40,0x80|((value>>6)&0x3f), 0xc0|(value&0x3f)])]
   end
 
   def exit_seq
-    set_flag = [nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil]
+    set_flag = [nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil]
     file = []
     file = IO.readlines(File_ana) if File.exist?( File_ana )
 
@@ -495,6 +623,12 @@ p [__LINE__, @spi.dataRW([0x29,0x40,0x80|((value>>6)&0x3f), 0xc0|(value&0x3f)])]
       when 'meas_ed2'
         fw << "meas_ed2=#{@entMeasEd2.text.to_i}\n"
         set_flag[16] = true
+      when 'shutter_init'
+        fw << "shutter_init=#{@entInit.text.to_i}\n"
+        set_flag[17] = true
+      when 'shutter_pos'
+        fw << "shutter_pos=#{@entShtPos.text.to_i}\n"
+        set_flag[18] = true
       else
         fw << line
       end
@@ -517,6 +651,8 @@ p [__LINE__, @spi.dataRW([0x29,0x40,0x80|((value>>6)&0x3f), 0xc0|(value&0x3f)])]
     fw << "fact2_b=#{$fact2_b}\n" if set_flag[14] == nil
     fw << "meas_st2=#{@entMeasSt2.text.to_i}\n" if set_flag[15] == nil
     fw << "meas_ed2=#{@entMeasEd2.text.to_i}\n" if set_flag[16] == nil
+    fw << "shutter_init=#{@entInit.text.to_i}\n" if set_flag[17] == nil
+    fw << "shutter_pos=#{@entShtPos.text.to_i}\n" if set_flag[18] == nil
     fw.close
 
     Gtk.main_quit()
@@ -639,6 +775,52 @@ Gtk.timeout_add( 1000 ) do
     ary = w.spi.dataRW([0x0f,0x40,0x80,0xc0])
     w.lblPumpVal.set_text("#{ary[2]&0xf}") if ary[0]==1
   end
+
+  # シャッタ
+  if $shutter_run == 5
+    ary = w.spi.dataRW([0x16,0x40,0x80,0xc0])
+    if ary[0]==1 && (ary[1]&0x08)==0x00
+      $shutter_value[0] = (ary[0]==1) ? ary[3]&0x3f : 0
+      $shutter_value[1] = (ary[0]==1) ? ary[2]&0x3f : 0
+
+      if ($shutter_value[1]-$shutter_value[0])==25
+        # シャッタ動作開始
+        value = w.entShtPos.text.to_i
+        #value = w.entDelay.text.to_i + w.entCntTime.text.to_i
+        w.spi.dataRW([0x36,0x41,0x80|((value>>6)&0x3f),0xc0|(value&0x3f)])
+        w.lblStSts.set_text("Run 開2:#{$shutter_value[1]} 開1:#{$shutter_value[0]}")
+        style = Gtk::Style.new
+        style.set_fg(Gtk::STATE_NORMAL, 0, 0, 65535)
+        w.lblStSts.style = style
+        w.btnStop.set_label('停止')
+      else
+        w.lblStSts.set_text("Fail 開2:#{$shutter_value[1]} 開1:#{$shutter_value[0]}")
+        style = Gtk::Style.new
+        style.set_fg(Gtk::STATE_NORMAL, 65535, 0, 0)
+        w.lblStSts.style = style
+        w.btnStop.set_label('シャッタ動作')
+      end
+      $shutter_run = nil
+    elsif ary[0]!=1
+      w.lblStSts.set_text('Error')
+      style = Gtk::Style.new
+      style.set_fg(Gtk::STATE_NORMAL, 65535, 0, 0)
+      w.lblStSts.style = style
+
+      $shutter_run = nil
+      $shutter_value[0] = 0
+      $shutter_value[1] = 0
+      w.btnStop.set_label('シャッタ動作')
+    end
+  elsif $shutter_run
+    w.lblStSts.set_text('イニシャル中')
+    style = Gtk::Style.new
+    style.set_fg(Gtk::STATE_NORMAL, 0, 0, 0)
+    w.lblStSts.style = style
+    $shutter_run += 1
+
+  end
+
   true
 end
 
